e43f840550f4a558f1272ee04f7e503d879aea4e
nigarhaqverdiyeva <nigarah-ap215@code.edu.az>
2024-10-06 19:00:22 +0400

    Add files via upload

---
+++ AB108(HOME)/ARRAY/ARRAY/Program.cs
@@ -0,0 +1,28 @@
+﻿//int[] numbers = { 0,5, 7, 8, 12, 34, 26, 89 };
+//for (int i = 0;i<numbers.Length;i++)
+//{
+//    if (numbers[i]<10)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//}                            Task 3 (home)
+
+//int num = 456789012;
+//int counter = 0;
+//while (num>0)
+//{
+//    int digit = num % 10;
+//    num=num/10;
+//    counter++;
+//}
+//Console.WriteLine(counter);       Task2 (home)
+
+
+//int a = 2;
+//int b=2;
+//int power = 1;
+//for (int i = 0; i <b; i++)
+//{
+//        power *= a;
+//}
+//Console.WriteLine(power);           Task1 (home)
