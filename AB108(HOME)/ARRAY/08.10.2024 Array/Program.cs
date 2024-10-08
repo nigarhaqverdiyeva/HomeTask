90af11d002b2b967b49aca69a87c72f9739eb91d
nigarhaqverdiyeva <nigarah-ap215@code.edu.az>
2024-10-09 00:37:37 +0400

    Added

---
+++ AB108(HOME)/ARRAY/08.10.2024 Array/Program.cs
@@ -0,0 +1,37 @@
+﻿//Verilmiş 2 integer array-dən birincisində yer alıb digərində yer almayan elementləri ekrana çıxarın
+
+//Verilmiş integer array-in tək indexdə duran elementləri ilə cüt indexdə duran elementləri müqayisə edin
+//int[] arr = {1,2,3,4,5,6,7,8,9,10};
+//int[] arr1 = { 3, 5, 7, 9, 10,11,12,13,14,15 };
+//for (int i = 0;i < arr.Length;i++)
+//{
+//bool find=true;
+//    for (int j = 0;j < arr1.Length;j++)
+//    {
+//        if (arr[i] == arr1[j])
+//        {
+//            find = false;
+//            break;
+//        }
+//    }
+//   if (find)
+//    {
+//        Console.WriteLine(arr[i]);
+//    }
+//}                                  Task 1
+
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9,10 };
+//int a = arr.Length;
+//for (int i = 0; i <= arr.Length; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        if (i + 1 < a)
+//        {
+//            Console.WriteLine("Cut indeksler---" +arr[i]) ;
+//            Console.WriteLine("Tek indeksler---" +arr[i+1]);
+//        }
+
+//    }
+//}                                 task2
