86449d3277a685999dbd57d1066d64899f889843
nigarhaqverdiyeva <nigarah-ap215@code.edu.az>
2024-10-15 00:06:21 +0400

    Add files via upload

---
+++ 14102024 Class/14102024 Class/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace _14102024_Class;
+
+public class Product
+{
+    public  string Name;
+    public string BrandName;
+    public double Price;
+    public double minPrice;
+    double maxPrice;
+
+
+
+}
+
---
+++ 14102024 Class/14102024 Class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _14102024_Class
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[]
+            {
+                new Product
+                {
+                    Name = "Rice",
+                    BrandName = "Tamasha",
+                    Price = 23.4
+                },
+                new Product
+                {
+                    Name = "Apple",
+                    BrandName = "Guba",
+                    Price = 44
+                }
+            };
+
+            double minPrice = 30.0;
+            double maxPrice = 50.0;
+
+            ProductMethod(products, minPrice, maxPrice);
+        }
+
+        static void ProductMethod(Product[] products, double minPrice, double maxPrice)
+        {
+            foreach (var product in products)
+            {
+                if (product.Price >= minPrice && product.Price <= maxPrice)
+                {
+                    Console.WriteLine("Ad: " + product.Name);
+                    Console.WriteLine("Qiymət: " + product.Price);
+                }
+            }
+        }
+    }
+}
