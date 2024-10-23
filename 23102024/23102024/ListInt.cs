426ae5c72d5c02e23e53cc532e722372be7aba38
nigarhaqverdiyeva <nigarah-ap215@code.edu.az>
2024-10-24 01:05:39 +0400

    Add files via upload

---
+++ 23102024/23102024/ListInt.cs
@@ -0,0 +1,137 @@
+﻿namespace _23102024;
+public class ListInt
+{
+    private int[] array;
+    private int size;
+
+    public ListInt()
+    {
+        array = new int[10]; 
+        size = 0;
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= size)
+                return 0;
+            return array[index];
+        }
+        set
+        {
+            if (index < 0 || index >= size)
+            {
+                return;
+            }
+            array[index] = value;
+        }
+    }
+        public void Add(int num)
+    {
+        if (size >= array.Length)
+        {
+            int newSize = array.Length * 2; 
+            int[] newArray = new int[newSize];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i]; 
+            }
+            array = newArray; 
+        }
+
+        array[size] = num; 
+        size++; 
+    }
+
+    public void Add(params int[] nums)
+    {
+        foreach (var num in nums)
+        {
+            Add(num); 
+        }
+    }
+     public bool Contains(int num)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] == num)
+                return true;
+        }
+        return false;
+    }
+    public int Sum()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < size; i++)
+        {
+            result += array[i] + " ";
+        }
+        return result.Trim();
+    }
+
+    public int IndexOf(int num)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] == num)
+                return i;
+        }
+        return -1;
+    }
+
+    public int LastIndexOf(int num)
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            if (array[i] == num)
+                return i;
+        }
+        return -1; 
+    }
+    public void Insert(int num, int index)
+    {
+        if (index < 0 || index > size)
+            return; 
+
+        if (size >= array.Length)
+        {
+            int newSize = array.Length * 2; 
+            int[] newArray = new int[newSize];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray; 
+        }
+
+        for (int i = size; i > index; i--)
+        {
+            array[i] = array[i - 1];
+        }
+
+        array[index] = num; 
+        size++; 
+    }
+    public float Average()
+    {
+        if (size == 0)
+        {
+            return 0; 
+        }
+        return (float)Sum() / size;
+    }
+}
+
+
+
---
+++ 23102024/23102024/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _23102024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt list = new ListInt();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3, 4, 5);
+            list.Add(6, 7, 8, 9);
+            Console.WriteLine("List" + "-" + " " + list.ToString());
+            list.Insert(10, 2);
+            Console.WriteLine("Insert" + "-" + " " + list.ToString());
+            Console.WriteLine("Sum " + "-" + " " + list.Sum());
+            Console.WriteLine("Average" + "-" + " " + list.Average());
+            Console.WriteLine("Contains" + "-" + " " + list.Contains(9));
+            Console.WriteLine("Index of" + "-" + " " + list.IndexOf(4));
+            Console.WriteLine("Last Index of" + "-" + " " + list.LastIndexOf(5));
+            Console.WriteLine("List " + "-" + " " + list.ToString());
+        }
+    }
+}
